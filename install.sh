197a28f866ab1faab15ed8540c79b1bec4799b78
Chris McKenzie <kristopolous@yahoo.com>
2016-12-07 11:41:35 +0000

    adding the apt-get lines to the installer

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -x
+apt-get install -y python-netaddr python-scapy tcpdump
 mkdir -p /var/log/probemon
 cp configs/logrotate /etc/logrotate.d/probemon
 cp configs/initd /etc/init.d/probemon
