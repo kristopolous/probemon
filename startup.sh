cbb8642213f98ac10b7a7b40dcfd27deeaf96f7c
Chris McKenzie <kristopolous@yahoo.com>
2016-12-30 09:47:57 +0000

    Single dev probing

--- startup.sh
+++ startup.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 cd /var/log/probemon
-service ifplugd stop
+# service ifplugd stop
 
-for id in 0 1 2 ; do
-	dev=wlan$id
-	ifconfig $dev down
-	iwconfig $dev mode monitor
-	ifconfig $dev up
-	/home/pi/probemon/probemon.py -r -t unix -i $dev -o probemon-dev-$id.log &
-done
+dev=wlan0
+ifconfig $dev down
+iwconfig $dev mode monitor
+ifconfig $dev up
+exec /home/pi/probemon/probemon.py -r -t unix -i $dev -o probemon-dev-0.log 
