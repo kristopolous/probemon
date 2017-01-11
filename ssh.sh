5d7fe7ad383ebeebf7c47a8036af8cf572acb9f9
Chris McKenzie <kristopolous@yahoo.com>
2017-01-11 15:18:52 -0800

    fixing a few things

---
+++ ssh.sh
@@ -0,0 +1,1 @@
+ssh -NC -R 9ol.es:4301:0.0.0.0:22 chris@9ol.es
--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+set -x
 cd /var/log/probemon
 # service ifplugd stop
 
@@ -6,4 +7,15 @@ dev=wlan0
 ifconfig $dev down
 iwconfig $dev mode monitor
 ifconfig $dev up
-exec /home/pi/probemon/probemon.py -r -t unix -i $dev -o probemon-dev-0.log 
+/home/pi/probemon/probemon.py -r -t unix -i $dev -o probemon-dev-0.log &
+
+while [ 0 ]; do
+  scp -C /var/log/probemon/probemon-dev-0.log chris@9ol.es:logs/
+  sleep 10
+  ntpdate pool.ntp.org
+
+  sleep 30
+  ntpdate pool.ntp.org
+
+  sleep 590
+done
