6e94640a1473ed59f4eac9234faf3ef78c24ad22
Chris McKenzie <kristopolous@yahoo.com>
2017-03-01 02:30:51 -0800

    Adding an ability to have an uniq id + rsync

--- startup.sh
+++ startup.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 set -x
 cd /var/log/probemon
+dev=wlan8
+#SRCHOME=/home/pi/probemon
+SRCHOME=/home/chris/code/probemon
 # service ifplugd stop
 
-dev=wlan0
 ifconfig $dev down
 iwconfig $dev mode monitor
 ifconfig $dev up
-/home/pi/probemon/probemon.py -r -t unix -i $dev -o probemon-dev-0.log &
+
+$SRCHOME/probemon.py -r -t unix -i $dev -o probemon-dev-0.log &
+
+# /dev/urandom needs to "build up" entropy for uuidgen ...
+# this is obscure, but true ... might as well give it some space.
+if [ ! -e whoami ]; then
+  uuidgen > whoami
+fi
+
+whoami=`cat whoami`
 
 while [ 0 ]; do
-  scp -C /var/log/probemon/probemon-dev-0.log chris@9ol.es:logs/
+  rsync -avzr /var/log/probemon/ chris@9ol.es:logs/$whoami
   sleep 10
   ntpdate pool.ntp.org
 
