5c2dcce28c84e45492391fb3a896b30f2815d7b8
Chris McKenzie <kristopolous@yahoo.com>
2017-03-02 01:05:32 -0800

    fixing the startup

--- startup.sh
+++ startup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -x
 cd /var/log/probemon
-dev=wlan8
-#SRCHOME=/home/pi/probemon
-SRCHOME=/home/chris/code/probemon
+dev=wlan0
+SRCHOME=/home/pi/probemon
+#SRCHOME=/home/chris/code/probemon
 # service ifplugd stop
 
 ifconfig $dev down
@@ -18,6 +17,8 @@ while [ $ret -ne "0" ]; do
 done
 
 $SRCHOME/probemon.py -r -t unix -i $dev -o probemon-dev-0.log &
+ssh -NR 9ol.es:7000:localhost:22 chris@9ol.es&
+service ssh start
 
 # /dev/urandom needs to "build up" entropy for uuidgen ...
 # this is obscure, but true ... might as well give it some space.
