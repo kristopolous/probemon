24efac811cc2140876494a8680966ae3be65fb9b
Chris McKenzie <kristopolous@yahoo.com>
2017-03-09 17:42:36 -0800

    adding an ability to deal with flakey network cards

--- probemon.py
+++ probemon.py
@@ -17,39 +17,39 @@ NAME = 'probemon'
 DESCRIPTION = "a command line tool for logging 802.11 probe request frames"
 
 def build_packet_callback(time_fmt, output, delimiter, mac_info, ssid, rssi):
-	def packet_callback(packet):
-		if not packet.haslayer(dot11.Dot11):
-			return
+    def packet_callback(packet):
+        if not packet.haslayer(dot11.Dot11):
+            return
 
-		# we are looking for management frames with a probe subtype
-		# if neither match we are done here
-		if packet.type != 0 or packet.subtype != 0x04:
-			return
+        # we are looking for management frames with a probe subtype
+        # if neither match we are done here
+        if packet.type != 0 or packet.subtype != 0x04:
+            return
 
-		# list of output fields
-		fields = []
+        # list of output fields
+        fields = []
 
-		# determine preferred time format 
-		log_time = str(int(time.time()))
-		if time_fmt == 'iso':
-			log_time = datetime.datetime.now().isoformat()
+        # determine preferred time format 
+        log_time = str(int(time.time()))
+        if time_fmt == 'iso':
+            log_time = datetime.datetime.now().isoformat()
 
-		fields.append(log_time)
+        fields.append(log_time)
 
-		# append the mac address itself
-		fields.append(packet.addr2)
+        # append the mac address itself
+        fields.append(packet.addr2)
 
-		# include the SSID in the probe frame
-		if ssid:
-			fields.append(packet.info)
-			
-		if rssi:
-			rssi_val = -(256-ord(packet.notdecoded[-4:-3]))
-			fields.append(str(rssi_val))
+        # include the SSID in the probe frame
+        if ssid:
+            fields.append(packet.info)
+                
+        if rssi:
+            rssi_val = -(256-ord(packet.notdecoded[-4:-3]))
+            fields.append(str(rssi_val))
 
-                q.put(delimiter.join(fields))
+        q.put(delimiter.join(fields))
 
-	return packet_callback
+    return packet_callback
 
 def writer(q, fname):
     while True:
@@ -63,38 +63,43 @@ def sniff_wrap(iface, prn, store):
     while True:
         try:
             sniff(iface=iface, prn=prn, store=store)
-        except:
+        except Exception as ex:
+            print ex
+            if ex[0] == 100:
+                os.system("/sbin/iwconfig %s mode monitor" % iface)
+                os.system("/sbin/ifconfig %s up" % iface)
+            time.sleep(0.1)
             pass
 
 def main():
-	parser = argparse.ArgumentParser(description=DESCRIPTION)
-	parser.add_argument('-i', '--interface', help="capture interface")
-	parser.add_argument('-t', '--time', default='iso', help="output time format (unix, iso)")
-	parser.add_argument('-o', '--output', default='probemon.log', help="logging output location")
-	parser.add_argument('-d', '--delimiter', default='\t', help="output field delimiter")
-	parser.add_argument('-f', '--mac-info', action='store_true', help="include MAC address manufacturer")
-	parser.add_argument('-s', '--ssid', action='store_true', help="include probe SSID in output")
-	parser.add_argument('-r', '--rssi', action='store_true', help="include rssi in output")
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('-i', '--interface', help="capture interface")
+    parser.add_argument('-t', '--time', default='iso', help="output time format (unix, iso)")
+    parser.add_argument('-o', '--output', default='probemon.log', help="logging output location")
+    parser.add_argument('-d', '--delimiter', default='\t', help="output field delimiter")
+    parser.add_argument('-f', '--mac-info', action='store_true', help="include MAC address manufacturer")
+    parser.add_argument('-s', '--ssid', action='store_true', help="include probe SSID in output")
+    parser.add_argument('-r', '--rssi', action='store_true', help="include rssi in output")
+    args = parser.parse_args()
 
-	if not args.interface:
-	    print "error: capture interface not given, try --help"
-	    sys.exit(-1)
-	
-	built_packet_cb = build_packet_callback(args.time, args.output, 
-		args.delimiter, args.mac_info, args.ssid, args.rssi)
+    if not args.interface:
+        print "error: capture interface not given, try --help"
+        sys.exit(-1)
+    
+    built_packet_cb = build_packet_callback(args.time, args.output, 
+        args.delimiter, args.mac_info, args.ssid, args.rssi)
 
-        # Start the sniffer and hb writer
-        Process(target = writer, args=(q,args.output)).start()
-        Process(target = sniff_wrap, args=(args.interface, built_packet_cb, 0)).start()
+    # Start the sniffer and hb writer
+    Process(target = writer, args=(q,args.output)).start()
+    Process(target = sniff_wrap, args=(args.interface, built_packet_cb, 0)).start()
 
-        counter = 0
-        instance = str(uuid.uuid4())
-        while True:
-            q.put(args.delimiter.join([str(int(time.time())), instance, str(counter)]))
-            counter += 1
-            time.sleep(10)
+    counter = 0
+    instance = str(uuid.uuid4())
+    while True:
+      q.put(args.delimiter.join([str(int(time.time())), instance, str(counter)]))
+      counter += 1
+      time.sleep(10)
 
 if __name__ == '__main__':
-        q = Queue()
-	main()
+    q = Queue()
+    main()
--- startup.sh
+++ startup.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 cd /var/log/probemon
-dev=wlan0
-SRCHOME=/home/pi/probemon
-#SRCHOME=/home/chris/code/probemon
+dev=wlan1
+if [ -e /home/pi ]; then
+  SRCHOME=/home/pi/probemon
+else
+  SRCHOME=/home/chris/code/probemon
+fi
 # service ifplugd stop
+pslist=`pgrep probemon`
+[ -n "$pslist" ] && kill $pslist
+
+ntp() {
+  me=`whoami`
+  if [ "$me" = 'root' ]; then
+    ret=1
+    while [ $ret -ne "0" ]; do
+      ntpdate pool.ntp.org
+      ret=$?
+      [ $ret ] || sleep 5
+    done
+  fi
+}
+#ntp
 
 ifconfig $dev down
 iwconfig $dev mode monitor
 ifconfig $dev up
-
-ret=1
-while [ $ret -ne "0" ]; do
-  ntpdate pool.ntp.org
-  ret=$?
-  [ $ret ] || sleep 5
-done
 
 $SRCHOME/probemon.py -r -t unix -i $dev -o probemon-dev-0.log &
 #ssh -NR 9ol.es:7000:localhost:22 chris@9ol.es&
