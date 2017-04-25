9f18106fdf8fa5824797883d35d2982bfefbde2a
Chris McKenzie <kristopolous@yahoo.com>
2017-04-24 18:18:32 -0700

    better remediation for no time stamps

--- startup.sh
+++ startup.sh
@@ -2,6 +2,7 @@
 cd /var/log/probemon
 dev=wlan0
 SRCHOME=/home/pi/probemon
+LOG=probemon-dev-0.log
 #SRCHOME=/home/chris/code/probemon
 # service ifplugd stop
 
@@ -9,6 +10,7 @@ ifconfig $dev down
 iwconfig $dev mode monitor
 ifconfig $dev up
 
+# Try to sync with the NTP server
 ret=1
 attempts=20
 while [ $ret -ne "0" -a $attempts -gt "0" ]; do
@@ -22,9 +24,18 @@ while [ $ret -ne "0" -a $attempts -gt "0" ]; do
   fi
 done
 
-$SRCHOME/probemon.py -r -t unix -i $dev -o probemon-dev-0.log &
-#ssh -NR 9ol.es:7000:localhost:22 chris@9ol.es&
-#service ssh start
+# If this fails then we just push our timestamps 3
+# minutes forward from the last recorded time. The 
+# focus here is that we don't want overlap.  This
+# policy may be (ok, it's very likely) incorrect
+# but it's the best we can do
+if [ $attempts -eq "0" -a -s $LOG ]; then
+  lasttime=`tail -1 $LOG | awk ' { print $1 } '`
+  newtime=$(( lasttime + 180 ))
+  date -s @$newtime
+fi
+  
+$SRCHOME/probemon.py -r -t unix -i $dev -o $LOG &
 
 # /dev/urandom needs to "build up" entropy for uuidgen ...
 # this is obscure, but true ... might as well give it some space.
@@ -36,5 +47,15 @@ whoami=`cat whoami`
 
 while [ 0 ]; do
   rsync -avzr /var/log/probemon/ chris@9ol.es:logs/$whoami
+
   sleep 600
+
+  # If we're here this means that we initially timed out
+  # in an attempt to find the network. More than likely the
+  # rsync above totally failed. But we'll try in vain to
+  # get the more accurate timestamp, hoping that eventually
+  # we will see the internet again.
+  if [ $attempts -eq "0" ]; then
+    ntpdate pool.ntp.org >& /dev/null
+  fi
 done
